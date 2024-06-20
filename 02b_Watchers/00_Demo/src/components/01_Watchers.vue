8925e73a9d81cfd0a656907d118d614796ab3a9c
dgaravaldi <daniel.garavaldi@gmail.com>
2024-06-20 11:24:29 +0200

    Me21e, 02b

---
+++ 02b_Watchers/00_Demo/src/components/01_Watchers.vue
@@ -0,0 +1,43 @@
+<!--
+  Using Watchers to Set New Values
+-->
+<template>
+  <div class="container">
+    <h1>Shop Watcher</h1>
+    <div>
+      Black Friday sale
+      <strike></strike>
+      <strong></strong><br/>
+      <a>Increase discount</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  padding: 30px;
+  max-width: 600px;
+  font-family: 'Avenir', Helvetica, sans-serif;
+  margin: 0;
+}
+
+a {
+  display: inline-block;
+  background: rgb(235, 50, 50);
+  border-radius: 10px;
+  font-size: 14px;
+  color: white;
+  padding: 10px 20px;
+  text-decoration: none;
+}
+</style>
---
+++ 02b_Watchers/00_Demo/src/components/02_NestedWatchers.vue
@@ -0,0 +1,44 @@
+<!--
+  Watching Nested Properties of a Data Object
+-->
+<template>
+  <div class="container">
+    <h1>Price Watcher</h1>
+    <div>
+      <h4></h4>
+      <h5></h5>
+      <a></a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  padding: 30px;
+  max-width: 600px;
+  font-family: 'Avenir', Helvetica, sans-serif;
+  margin: 0;
+}
+a {
+  display: inline-block;
+  background: rgb(235, 50, 50);
+  border-radius: 10px;
+  font-size: 14px;
+  color: white;
+  padding: 10px 20px;
+  text-decoration: none;
+}
+</style>
+Set up a butto
